a91a6f09a783477e302090504a8255af094e9217
junis <jupia3346@naver.com>
2024-01-04 13:09:15 +0900

    모바일 반응형

--- js/background.js
+++ js/background.js
@@ -7,6 +7,7 @@ const chosenImage = images[Math.floor(Math.random()*images.length)]
 // html에 이미지 추가함
 const bgImage = document.createElement('img');
 bgImage.src = `./img/${chosenImage}`
+bgImage.id = 'bgImg';
 
 // prepend는 맨앞에 위치하도록하는것 
 document.body.prepend(bgImage)
